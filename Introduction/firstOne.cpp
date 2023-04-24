d719b6d551b4c39172710eca9200794be5f7b23a
RISHI6644\RISHI <hrishikeshdas111@gmail.com>
2023-04-24 19:04:59 +0600

    Intro code and gitingore added

---
+++ Introduction/firstOne.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    freopen("input.txt", "r", stdin);   // can need to change file . this one for taking input
+    freopen("output.txt", "w", stdout); // this one for output
+
+    // Your Code
+
+    long long int s;
+
+    cin >> s;
+
+    cout << s;
+
+    return 0;
+}
